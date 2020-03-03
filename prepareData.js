1eb8c4cb1fdcd55f991168542979bead3955cb32
Neil Blake <neil0blake@gmail.com>
2020-03-03 21:50:31 +1300

    2019 dataset - csv format for compression (dataset too large for free tier db...)

--- prepareData.js
+++ prepareData.js
@@ -1,30 +1,53 @@
 const fs = require("fs");
+const stringify = require("csv-stringify");
 
 const sourceFile = process.argv[2] || "./data/crashDataRaw.csv";
 const outputFile = process.argv[3] || "./data/crashes.csv";
 
 const fileOutputStream = fs.createWriteStream(outputFile);
 
-//ignore these fields for compression/simplicity
-const ignoreFields = new Set([
-  "urban",
-  "outdatedLocationDescription",
-  "crashDirectionDescription",
-  "roadSurface",
-  "meshblockId",
-  "areaUnitID",
-  "northing",
-  "easting",
-  "directionRoleDescription",
-  "tlaId",
-  "tlaName",
-  "crashFinancialYear"
-]);
+const includeFields = [
+  "id",
+  "crashYear",
+  "crashSeverity",
+  "fatalCount",
+  "seriousInjuryCount",
+  "minorInjuryCount",
+  "crashLocation1",
+  "crashLocation2",
+  "region",
+  "holiday",
+  "light",
+  "speedLimit",
+  "advisorySpeed",
+  "latitude",
+  "longitude",
+  "bicycle",
+  "bus",
+  "carStationWagon",
+  "moped",
+  "motorcycle",
+  "otherVehicleType",
+  "pedestrian",
+  "schoolBus",
+  "suv",
+  "taxi",
+  "train",
+  "truck",
+  "vanOrUtility",
+  "vehicle"
+];
+
+//reordering columns
+const includeFieldsIndexMapper = {};
+includeFields.forEach((val, index) => {
+  includeFieldsIndexMapper[val] = index;
+});
 
 //rounding for some compression
 const valueTransformer = {
-  X: val => Number(val).toFixed(6),
-  Y: val => Number(val).toFixed(6)
+  longitude: val => Number(val).toFixed(6),
+  latitude: val => Number(val).toFixed(6)
 };
 
 //improving namings from source
@@ -51,37 +74,48 @@ const lineReader = require("readline").createInterface({
 });
 
 let csvColumnMappings = {};
-
 lineReader.on("line", function(line) {
-  const result = [];
+  let result = new Array(includeFields.length);
   if (row === 0) {
-    let index = 0;
-
+    columnIndex = 0;
     for (const rawColumn of line.split(",")) {
       //cleaning column
       let column = rawColumn.trim();
       column = fieldTransformer[column] || column;
-      if (!ignoreFields.has(column)) {
-        csvColumnMappings[column] = index;
-        result.push(column);
-      }
-      index++;
+      csvColumnMappings[column] = columnIndex;
+      columnIndex++;
     }
+    result = [...includeFields];
   } else {
     const data = line.split(",");
     // ignoring non-injury crashes for now.
     if (data[csvColumnMappings.crashSeverity] === "Non-Injury Crash") {
       return;
     }
-    for (const column of Object.keys(csvColumnMappings)) {
-      const index = csvColumnMappings[column];
-      let cell = ignoreItems.has(data[index]) ? "" : data[index];
+    for (const column of Object.keys(includeFieldsIndexMapper)) {
+      const sourceIndex = csvColumnMappings[column];
+      const resultIndex = includeFieldsIndexMapper[column];
+      let cell = ignoreItems.has(data[sourceIndex]) ? "" : data[sourceIndex];
       if (cell && valueTransformer[column]) {
         cell = valueTransformer[column](cell);
       }
-      result.push(cell);
+
+      result[resultIndex] = cell;
     }
   }
   row++;
-  fileOutputStream.write(result.join(",") + "\n");
+
+  //Need to deal with quotes and commas in strings for csv
+  stringify([result], function(err, output) {
+    if (err) {
+      console.log(
+        "Unexpected error stringifying result: ",
+        result,
+        "error is",
+        err
+      );
+      return;
+    }
+    fileOutputStream.write(output);
+  });
 });
